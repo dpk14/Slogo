da6e8a2db48c7a92002114b0f5fdd43bfa869de9
Diego Chamorro <dfc15@duke.edu>
2019-02-21 17:34:59 -0500

    added APi_REVIEW

--- src/Console.java
+++ src/Console.java
@@ -1,9 +1,17 @@
+import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
+import javafx.scene.layout.HBox;
 
 public class Console {
     TextArea userInput;
+    HBox consoleArea;
+    Button implement;
     public Console() {
+        consoleArea = new HBox();
         userInput = new TextArea();
+        implement = new Button();
+
+
     }
 
 
