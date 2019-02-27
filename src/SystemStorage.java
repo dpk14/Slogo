554963cd1904aa5fc2563fa01aac755b98ab747a
Diego Chamorro <diegochll@gmail.com>
2019-02-26 21:08:06 -0500

    updated animal, animalScreen, ScreenBuilder, ScreenOptions, AnimalScreen,;

--- src/SystemStorage.java
+++ src/SystemStorage.java
@@ -1,25 +1,25 @@
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 public class SystemStorage {
 
     Map<String, Animal> habitat;
+
     public SystemStorage(){
         habitat = new HashMap<String, Animal>();
     }
 
-
-
-
-
-
-
-    public void storeAnimal(String animalName, Animal object){
-        habitat.put(animalName, object);
+    public void storeAnimal(String animalName, Animal animal){
+        habitat.put(animalName, animal);
     }
 
     public Animal getAnimal(String animalName){
         return habitat.get(animalName);
+    }
+
+    public Set<String> getAnimalNames(){
+        return habitat.keySet();
     }
 
 }
