8ff2888c1e3ee2982e98514195b0ba43425c6e18
Diego Chamorro <diegochll@gmail.com>
2019-02-26 21:14:07 -0500

    added console, and images for animal

--- src/Console.java
+++ src/Console.java
@@ -1,21 +1,29 @@
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
-import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 
 public class Console {
     TextArea userInput;
-    HBox consoleArea;
+    VBox consoleArea;
     Button implement;
-    public Console() {
-        consoleArea = new HBox();
-        userInput = new TextArea();
-        implement = new Button();
 
+    public Console() {
+        consoleArea = new VBox();
+        userInput = new TextArea();
+        userInput.setWrapText(true);
+        implement = new Button("Run");
+        implement.setOnAction(e->sendText());
+        consoleArea.getChildren().addAll(userInput, implement);
+
+    }
+
+    private void sendText(){
+        String input = userInput.getText();
 
     }
 
 
-    public TextArea getTextArea(){
-        return userInput;
+    public VBox getConsoleArea(){
+        return consoleArea;
     }
 }
