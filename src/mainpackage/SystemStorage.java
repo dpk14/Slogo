a3285456a630019926789dd38d1a6e22dc361579
Irene Qiao <isq@duke.edu>
2019-03-05 21:40:30 -0500

    resolve merge conflict

--- src/mainpackage/SystemStorage.java
+++ src/mainpackage/SystemStorage.java
@@ -23,21 +23,14 @@ public class SystemStorage {
         myActiveAnimals = new ArrayList<>();
     }
 
-<<<<<<< HEAD
-    public void setActiveAnimals(ArrayList<String> activateAnimal){
-        for(String animalID : activateAnimal){
+    public void setActiveAnimals(ArrayList<String> activateAnimal) {
+        for (String animalID : activateAnimal) {
             myActiveAnimals.add(new AbstractMap.SimpleEntry<String, Animal>(animalID, getAnimal(animalID)));
-=======
+        }
+    }
 
     public Map<String, Animal> getAnimals(){
         return habitat;
-    }
-
-    public void setActiveAnimals(ArrayList<Integer> activateAnimal){
-        for(Integer animalID : activateAnimal){
-            myActiveAnimals.add(new AbstractMap.SimpleEntry<>(animalID, getAnimal(animalID)));
->>>>>>> a49de7723d9e8768420b4ecff96bd45fd8a792cc
-        }
     }
 
     public List<Map.Entry<String, Animal>> getActiveAnimals(){
